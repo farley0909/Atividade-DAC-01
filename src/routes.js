9d4f622ddb5e87e437d7391b8630f8edc1fdc55c
José Farley Sousa da Silva <202012010019@ad.ifpb.edu.br>
2023-03-13 10:56:21 -0300

    Enviando a base do back-end

---
+++ src/routes.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+
+const router = Router();
+
+//Book's routes
+router.post('/api/v1/book', async ()=>{
+
+})
+router.delete('/api/v1/book', async ()=>{
+    
+})
+router.update('/api/v1/book', async ()=>{
+    
+})
+//Fetch one book
+router.get('/api/v1/book/by-id', async ()=>{
+    
+})
+//Fecth all books
+router.get('/api/v1/book', async ()=>{
+    
+})
